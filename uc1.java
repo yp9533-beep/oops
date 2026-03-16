ab87d758e7e82dcd337e57049cfe2e7a84662203
yp9533-beep <yp9533@srmist.edu.in>
2026-03-16 17:42:41 +0530

    Add files via upload

---
+++ uc1.java
@@ -0,0 +1,12 @@
+public class Hello {
+
+    public static void main(String[] args) {
+
+        if(args.length > 0){
+            System.out.println("Hello " + args[0]);
+        } else {
+            System.out.println("Hello User");
+        }
+
+    }
+}
