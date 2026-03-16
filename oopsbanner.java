0cf7fb978fdd138c0571718e39f82877b0aae5e7
yp9533-beep <yp9533@srmist.edu.in>
2026-03-16 17:31:28 +0530

    Add files via upload

---
+++ oopsbanner.java
@@ -0,0 +1,10 @@
+public class oopsbanner{
+    public static void main (String [] args){
+        System.out.println(" OOOOO   OOOOO   PPPPP    SSSSS");
+        System.out.println("O     O O     O  P    P  S");
+        System.out.println("O     O O     O  PPPPP    SSSSS");
+        System.out.println("O     O O     O  P             S");
+        System.out.println(" OOOOO   OOOOO   P        SSSSS");
+        
+    }
+}
